c1553cb24b318ae386ab13c36ea204d7e60e7fb8
Rick <rlugtigheid77@gmail.com>
2019-10-30 10:14:53 +0100

    stabiele versie
    Ik heb de belangrijke dingen af nu nog een betaal menu maken zonder prompt om het mooier en makkelijker te maken

--- assets/js/motor.js
+++ assets/js/motor.js
@@ -1,50 +1,84 @@
 var drinkAmmount;
-var orders = [""];
+var orderList = [""];
 
 var totalPrize = 0;
 
-function addDrink(drinkName, prize){
+const formatter = new Intl.NumberFormat('it-NL', {
+    style: 'currency',
+    currency: 'EUR'
+  })
 
-        drinkAmmount = prompt("Hoeveel " + drinkName + " wilt U toevoegen aan uw bestelling?");
+//zet wat drinken bij de besteling
+function addDrink(Name, prize){
+
+        drinkAmmount = prompt("Hoeveel " + Name + " wilt U toevoegen aan uw bestelling?");
         drinkAmmount = parseFloat(drinkAmmount);
-        if(drinkAmmount != null){
-            addOrder(drinkName, drinkAmmount);
+        //zorgt dat je niet niks of een letter invoert
+        if(drinkAmmount != null && !isNaN(drinkAmmount)){
+            prize = prize * drinkAmmount;
+            orderList.push("Naam: " + Name + "<br> Hoeveelheid: " + drinkAmmount + "<br> Prijs: " + formatter.format(prize) +"<br><br>");//€
+            totalPrize += prize;
+        
+            updateOrder();
+        
+            console.log("NEW ORDER -> naam: " + Name + " Hoeveelheid: " + drinkAmmount);
         }else{
             alert("ERROR: uw invoer was incorrect!\n Voer een cijfer in.")
         }
 }
-
-function addOrder(orderName, orderAmmount, prize){
-    var prize;
-    if(orderName == "bier"){
-        prize = 2.50 * orderAmmount;
-    }else if(orderName == "sBier"){
-
-    }else if(orderName == "wijn"){
-        prize = 3.00 * orderAmmount;
-    }else if(orderName == "fris"){
-        prize = 2.00 * orderAmmount;
+//zet een portie snacks bij de bestelling
+function addPortion(Name, prize, portionSize){
+    portionAmmount = prompt("Hoeveel " + Name + " wilt U toevoegen aan uw bestelling?");
+    portionAmmount = parseFloat(portionAmmount);
+    if(portionAmmount != null && !isNaN(portionAmmount)){
+        prize = prize * portionAmmount;
+        orderList.push("Naam: " + Name + "<br> Hoeveelheid: " + portionAmmount + "porties, " + "(" + (portionAmmount * portionSize) + " bitter ballen)" + "<br> Prijs: " + formatter.format(prize) +"<br><br>");//€
+        totalPrize += prize;
+    
+        updateOrder();
+    
+        console.log("NEW ORDER -> naam: " + Name + " Hoeveelheid: " + portionAmmount);
+    }else{
+        alert("ERROR: uw invoer was incorrect!\n Voer een cijfer in.")
     }
-    orders.push("Naam: " + orderName + "<br> Hoeveelheid: " + orderAmmount + "<br> Prijs: $" + prize +"<br><br>");//€
-    totalPrize += prize;
-
-    updateOrder();
-
-    console.log("NEW ORDER -> naam: " + orderName + " Hoeveelheid: " + orderAmmount);
 }
 
-function deleteOrder(){
-    var orderToDelete = prompt("Welke bestelling wilt U verwijderen? \n Type het nummer van de bestelling in, dus voor de 3de bestelling typ een 3");
-
-    delete orders[orderToDelete];
-    updateOrder();
+function payOrder(){
+    var pay = prompt("Wilt U betalen met pin of contant?\n \nTyp 1 voor contant\n\nTyp 2 voor Pin\n\nTyp 3 om te annuleren");
+    if(pay == 1){
+        alert("De prijs die betaald moet worden is: " + formatter.format(totalPrize))
+        pay = prompt("Hoveel euro heeft U ontvangen?");
+        pay = parseFloat(pay);
+        if(pay != NaN && pay >= totalPrize){
+            alert("De klant krijgt nog " + formatter.format(totalPrize - pay) + " terug")
+            alert("De bestelling is betaald!");
+            document.getElementById("ORDER_LIST").innerHTML = "";
+            totalPrize = 0;
+            updateOrder();
+        }else if(pay <= totalPrize){
+            alert("ERROR: " + pay + "is lager dan de totale prijs. Totaale prijs: " + formatter.format(totalPrize));
+            payOrder();
+        }else{
+            alert("ERROR: " + pay + "is geen nummer");
+            payOrder();
+        }
+    }else if(pay == 2){
+        alert("Pin aparaat is actief");
+        //pin code hier
+    }else if(pay ==3){
+        
+    }else{
+        alert("ERROR: typ 1 of 2 om te kiezen");
+        payOrder();
+    }
 }
 
+//udate de bestelling, dus zorgt dat de bestelling klopt met wat is ingevoert
 function updateOrder(){
     document.getElementById("ORDER_LIST").innerHTML = "";
-    document.getElementById("TOTAL_PRIZE").innerHTML = "Totaal prijs: $" + totalPrize;
+    document.getElementById("TOTAL_PRIZE").innerHTML = "Totaal: " + formatter.format(totalPrize);
 
-    for(var i = 0; i <= (orders.length - 1); i++){
-        document.getElementById("ORDER_LIST").innerHTML += orders[i];
+    for(var i = 0; i <= (orderList.length - 1); i++){
+        document.getElementById("ORDER_LIST").innerHTML += orderList[i];
     }
 }
